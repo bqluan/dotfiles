fb8e6031fc21d9036e0237a4fca57975c51dc259
Ethan L <bingqian.luan@icloud.com>
2025-08-06 14:22:26 +0800

    nvim

---
+++ nvim/lua/plugins/builtins.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+          },
+          explorer = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+}
