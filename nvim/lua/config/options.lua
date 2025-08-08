9ae9b14fd955cf6a0e22e76b381f3df52887f81a
Ethan L <bingqian.luan@icloud.com>
2025-08-08 11:48:33 +0800

    snacks picker hidden file

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+-- Snacks picker:
+-- Make hidden files/directories appear the same as normal files in the picker
+vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "Text" })
