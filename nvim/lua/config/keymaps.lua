d281db91f094c63c8a0b8f1a9df412ab3ab04733
BQ <bingqian.luan@icloud.com>
2025-09-11 12:19:25 +0800

    fix(nvim): remove map of D-s because it's not supported in tmux

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Save file
-vim.keymap.set({ "n", "i", "v" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
