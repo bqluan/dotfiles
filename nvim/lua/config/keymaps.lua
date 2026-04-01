84bc45976a5700f883138b7d941cfd6bb1570f79
BQ <bingqian.luan@gmail.com>
2026-04-01 16:12:35 +0800

    refactor(nvim): reorganize keymaps with which-key groups and fix prefix conflicts
    - Add which-key group labels for <leader>b/c/f/g
    - Move format from <leader>f to <leader>cf, rename to <leader>cr
    - Reorganize keymaps by group (general/buffer/git/code/find/flash/window)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,17 @@
--- quit all (warns on unsaved buffers)
+-- which-key groups
+require("which-key").add({
+	{ "<leader>b", group = "Buffer" },
+	{ "<leader>c", group = "Code" },
+	{ "<leader>f", group = "Find" },
+	{ "<leader>g", group = "Git" },
+})
+
+-- general
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
-
--- save
 vim.keymap.set({ "n", "i" }, "<D-s>", "<cmd>write<cr>", { desc = "Save File" })
-
--- toggle file explorer
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Explorer" })
 
--- buffer navigation
+-- buffer
 vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
@@ -18,22 +22,22 @@ vim.keymap.set("n", "<leader>gg", function()
 	require("plugins.lazygit").open()
 end, { desc = "Lazygit" })
 
--- lsp
+-- code (lsp)
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local opts = { buffer = args.buf }
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Goto Definition" }))
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "References" }))
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover" }))
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename" }))
+		vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename" }))
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code Action" }))
-		vim.keymap.set("n", "<leader>f", function()
+		vim.keymap.set("n", "<leader>cf", function()
 			vim.lsp.buf.format()
 		end, vim.tbl_extend("force", opts, { desc = "Format" }))
 	end,
 })
 
--- fzf
+-- find
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Find Buffers" })
@@ -43,7 +47,6 @@ vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>", { desc = "Recent F
 vim.keymap.set({ "n", "x", "o" }, "s", function()
 	require("flash").jump()
 end, { desc = "Flash" })
-
 vim.keymap.set({ "n", "x", "o" }, "S", function()
 	require("flash").treesitter()
 end, { desc = "Flash Treesitter" })
